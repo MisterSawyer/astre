a2c9b54b339e803a9c5d1883b1b6683db6dcfb9a
MrSawyer <michal.skarzynski.0@gmail.com>
2026-07-03 01:40:47 +0200

    Bump spdlog 1.17.0

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -11,7 +11,7 @@ message(STATUS "Fetching dependency `spdlog` ...")
 FetchContent_Declare(
     spdlog
     GIT_REPOSITORY  "https://github.com/gabime/spdlog.git"
-    GIT_TAG         "v1.11.0"
+    GIT_TAG         "v1.17.0"
     SYSTEM
     OVERRIDE_FIND_PACKAGE
 )
